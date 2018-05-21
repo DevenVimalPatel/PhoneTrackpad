0f49f9e30b87106ce6a9c65667426f69525e8cc7
Sam Smith <Devenvimalpatel@gmail.com>
2018-05-21 13:51:26 -0700

    Work on creating a connection.

--- app/src/main/java/com/trackpad/aldini/phonetrackpad/Trackpad.java
+++ app/src/main/java/com/trackpad/aldini/phonetrackpad/Trackpad.java
@@ -6,13 +6,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Trackpad extends AppCompatActivity {
+import java.io.IOException;
+import java.net.Socket;
 
+public class Trackpad extends AppCompatActivity {
+    public static final String err = "error";
     private TextView mTextMessage;
+    private Socket toServer = null;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,13 +47,48 @@ public class Trackpad extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trackpad);
 
-        isConnected();
+        if(!isConnected()){
+            Log.e(err, "Device is not connected.");
+            //handleerror
+            finish();
+        }
+        if(!connectToServ()){
+            Log.e(err, "Connection to server failed.");
+            //handleerror
+            finish();
+        }
+
+        final Button button = findViewById(R.id.button);
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+    }
+
+    private boolean connectToServ(){
+        try {
+            toServer = new Socket("127.0.0.1", 8000);
+        } catch (IOException e) {
+            Log.e(err, "IOException thrown when connecting.");
+            //handleerror
+            finish();
+        }
+        return toServer != null;
     }
 
     private boolean isConnected(){
         ConnectivityManager connection =
                 (ConnectivityManager) this.getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
-        NetworkInfo info = connection.getActiveNetworkInfo();
+        NetworkInfo info = null;
+        try {
+            info = connection.getActiveNetworkInfo();
+        } catch(NullPointerException npe) {
+            Log.e(err, "Nullpointerexception thrown by NetworkInfo");
+            //handleerror
+            finish();
+        }
         if(info != null){
             if(info.getType() != ConnectivityManager.TYPE_WIFI){
                 Toast.makeText(this, info.getTypeName(), Toast.LENGTH_SHORT).show();
